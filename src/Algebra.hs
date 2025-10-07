126ed637c0d86bb1608c47dca340c0a1e402a968
Ruben <github@rubenbroere.com>
2025-10-07 12:59:20 +0200

    feat: algebra

---
+++ src/Algebra.hs
@@ -0,0 +1,105 @@
+module Algebra where
+
+import GCLParser.GCLDatatype hiding (opAnd, opEqual, opGreaterThan, opGreaterThanEqual, opImplication, opLessThan, opLessThanEqual, opOr)
+
+data ExprAlgebra r = ExprAlgebra
+  { onVar :: String -> r,
+    onLitI :: Int -> r,
+    onLitB :: Bool -> r,
+    onLitNull :: r,
+    onParens :: r -> r,
+    onArrayElem :: r -> r -> r,
+    onOpNeg :: r -> r,
+    onBinopExpr :: BinOp -> r -> r -> r,
+    onForall :: String -> r -> r,
+    onExists :: String -> r -> r,
+    onSizeOf :: r -> r,
+    onRepBy :: r -> r -> r -> r,
+    onCond :: r -> r -> r -> r,
+    onNewStore :: r -> r,
+    onDereference :: String -> r
+  }
+
+foldExpr :: ExprAlgebra r -> Expr -> r
+foldExpr f = go
+  where
+    go (Var v) = onVar f v
+    go (LitI x) = onLitI f x
+    go (LitB b) = onLitB f b
+    go LitNull = onLitNull f
+    go (Parens e) = onParens f (go e)
+    go (ArrayElem arr idx) = onArrayElem f (go arr) (go idx)
+    go (OpNeg e) = onOpNeg f (go e)
+    go (BinopExpr op e1 e2) = onBinopExpr f op (go e1) (go e2)
+    go (Forall var e) = onForall f var (go e)
+    go (Exists var e) = onExists f var (go e)
+    go (SizeOf e) = onSizeOf f (go e)
+    go (RepBy arr idx val) = onRepBy f (go arr) (go idx) (go val)
+    go (Cond cond thenE elseE) = onCond f (go cond) (go thenE) (go elseE)
+    go (NewStore e) = onNewStore f (go e)
+    go (Dereference var) = onDereference f var
+
+defaultExprAlgebra :: ExprAlgebra Expr
+defaultExprAlgebra =
+  ExprAlgebra
+    { onVar = Var,
+      onLitI = LitI,
+      onLitB = LitB,
+      onLitNull = LitNull,
+      onParens = Parens,
+      onArrayElem = ArrayElem,
+      onOpNeg = OpNeg,
+      onBinopExpr = BinopExpr,
+      onForall = Forall,
+      onExists = Exists,
+      onSizeOf = SizeOf,
+      onRepBy = RepBy,
+      onCond = Cond,
+      onNewStore = NewStore,
+      onDereference = Dereference
+    }
+
+data StmtAlgebra r = StmtAlgebra
+  { onSkip :: r,
+    onAssert :: Expr -> r,
+    onAssume :: Expr -> r,
+    onAssign :: String -> Expr -> r,
+    onAAssign :: String -> Expr -> Expr -> r,
+    onDrefAssign :: String -> Expr -> r,
+    onSeq :: r -> r -> r,
+    onIfThenElse :: Expr -> r -> r -> r,
+    onWhile :: Expr -> r -> r,
+    onBlock :: [VarDeclaration] -> r -> r,
+    onTryCatch :: String -> r -> r -> r
+  }
+
+foldStmt :: StmtAlgebra r -> Stmt -> r
+foldStmt f = go
+  where
+    go Skip = onSkip f
+    go (Assert expr) = onAssert f expr
+    go (Assume expr) = onAssume f expr
+    go (Assign var expr) = onAssign f var expr
+    go (AAssign var index expr) = onAAssign f var index expr
+    go (DrefAssign var expr) = onDrefAssign f var expr
+    go (Seq s1 s2) = onSeq f (go s1) (go s2)
+    go (IfThenElse guard s1 s2) = onIfThenElse f guard (go s1) (go s2)
+    go (While guard body) = onWhile f guard (go body)
+    go (Block vars body) = onBlock f vars (go body)
+    go (TryCatch e s1 s2) = onTryCatch f e (go s1) (go s2)
+
+defaultStmtAlgebra :: StmtAlgebra Stmt
+defaultStmtAlgebra =
+  StmtAlgebra
+    { onSkip = Skip,
+      onAssert = Assert,
+      onAssume = Assume,
+      onAssign = Assign,
+      onAAssign = AAssign,
+      onDrefAssign = DrefAssign,
+      onSeq = Seq,
+      onIfThenElse = IfThenElse,
+      onWhile = While,
+      onBlock = Block,
+      onTryCatch = TryCatch
+    }
--- src/Main.hs
+++ src/Main.hs
@@ -1,68 +1,84 @@
 {-# LANGUAGE LambdaCase #-}
+
 module Main where
 
+import Algebra
 import GCLParser.GCLDatatype
-import GCLParser.Parser ( parseGCLfile )
+import GCLParser.Parser (parseGCLfile)
 
 main :: IO ()
-main = parseGCLfile "examples/min.gcl" >>= \case
+main =
+  parseGCLfile "examples/min.gcl" >>= \case
     Left err -> putStrLn $ "Error parsing GCL file: " ++ err
     Right program -> do
-        putStrLn "Parsed GCL Program:"
-        print program
+      putStrLn "Parsed GCL Program:"
+      print program
 
-        let programStmt = stmt program
-        putStrLn "\nOriginal Statement:"
-        print programStmt 
+      let programStmt = stmt program
+      putStrLn "\nOriginal Statement:"
+      print programStmt
 
-        let filteredStmt = transformAsserts programStmt
-        putStrLn "\nFiltered Statement (without asserts):"
-        print filteredStmt
+      let filteredStmt = transformAsserts programStmt
+      putStrLn "\nFiltered Statement (without asserts):"
+      print filteredStmt
 
-        let wlpFormula = wlp filteredStmt (LitB True)  -- assuming postcondition is true
-        putStrLn "\nWLP Formula:"
-        print wlpFormula
-
+      let wlpFormula = wlp filteredStmt (LitB True) -- assuming postcondition is true
+      putStrLn "\nWLP Formula:"
+      print wlpFormula
 
 -- Filter out any assert statements from the program and convert assumes to asserts
 transformAsserts :: Stmt -> Stmt
-transformAsserts (Assert _) = Skip
-transformAsserts (Assume expr) = Assert expr
-transformAsserts (Seq s1 s2) = Seq (transformAsserts s1) (transformAsserts s2)
-transformAsserts (IfThenElse guard s1 s2) = IfThenElse guard (transformAsserts s1) (transformAsserts s2)
-transformAsserts (While guard body) = While guard (transformAsserts body)
-transformAsserts (Block vars body) = Block vars (transformAsserts body)
-transformAsserts (TryCatch e s1 s2) = TryCatch e (transformAsserts s1) (transformAsserts s2)
-transformAsserts stmt = stmt  -- For other statements, return as is
+transformAsserts =
+  foldStmt
+    defaultStmtAlgebra
+      { onAssert = const Skip,
+        onAssume = Assert
+      }
 
 wlp :: Stmt -> Expr -> Expr
-wlp Skip post = post
-wlp (Assign var expr) post = substitute var expr post
-wlp (Seq s1 s2) post = wlp s1 (wlp s2 post)
-wlp (Assert condition) post = opAnd condition post
-wlp (Assume condition) post = opImplication condition post
-wlp (IfThenElse guard s1 s2) post = opAnd (opImplication guard (wlp s1 post)) (opImplication (OpNeg guard) (wlp s2 post))
-wlp (While guard body) post = error "WLP for While loops not implemented yet"
-wlp stmt _ = error $ "WLP for " ++ show stmt ++ " not implemented yet"
+wlp =
+  foldStmt
+    StmtAlgebra
+      { onSkip = id,
+        onAssert = opAnd,
+        onAssume = opImplication,
+        onAssign = substitute,
+        onAAssign = \_ _ _ -> error "WLP for array assignments not implemented yet",
+        onDrefAssign = \_ _ -> error "WLP for dereference assignments not implemented yet",
+        onSeq = (.), -- function composition
+        onIfThenElse = \guard f1 f2 post -> opAnd (opImplication guard (f1 post)) (opImplication (OpNeg guard) (f2 post)),
+        onWhile = \_ _ -> error "WLP for While loops not implemented yet",
+        onBlock = \_ f -> f,
+        onTryCatch = \_ _ _ -> error "WLP for TryCatch not implemented yet"
+      }
+
+-- substituteAlgebra :: ExprAlgebra (String -> Expr -> Expr -> Expr)
+-- substituteAlgebra = undefined
 
 substitute :: String -> Expr -> Expr -> Expr
-substitute var new (Var v) | v == var = new
-                           | otherwise = Var v 
-substitute var new (BinopExpr And e1 e2) = opAnd (substitute var new e1) (substitute var new e2)
-substitute var new (BinopExpr Or e1 e2) = opOr (substitute var new e1) (substitute var new e2)
-substitute var new (BinopExpr Implication e1 e2) = opImplication (substitute var new e1) (substitute var new e2)
-substitute var new (OpNeg e) = OpNeg (substitute var new e)
-substitute var new (BinopExpr Equal e1 e2) = opEqual (substitute var new e1) (substitute var new e2)
-substitute var new (BinopExpr LessThan e1 e2) = opLessThan (substitute var new e1) (substitute var new e2)
-substitute var new (BinopExpr GreaterThan e1 e2) = opGreaterThan (substitute var new e1) (substitute var new e2)
-substitute var new (BinopExpr LessThanEqual e1 e2) = opLessThanEqual (substitute var new e1) (substitute var new e2)
-substitute var new (BinopExpr GreaterThanEqual e1 e2) = opGreaterThanEqual (substitute var new e1) (substitute var new e2)
-substitute _ _ post = post 
--- TODO: Is this correct? What about other Expr constructors?
+substitute var new post = foldExpr substituteAlgebra post var new
 
+substituteAlgebra :: ExprAlgebra (String -> Expr -> Expr)
+substituteAlgebra = ExprAlgebra
+  { onVar = \v var new -> if v == var then new else Var v,
+    onLitI = \i _ _ -> LitI i,
+    onLitB = \b _ _ -> LitB b,
+    onLitNull = \_ _ -> LitNull,
+    onParens = \e var new -> Parens (e var new),
+    onArrayElem = \arr index var new -> ArrayElem (arr var new) (index var new),
+    onOpNeg = \e var new -> OpNeg (e var new),
+    onBinopExpr = \op e1 e2 var new -> BinopExpr op (e1 var new) (e2 var new),
+    onForall = \v e var new -> if v == var then Forall v new else Forall v (e var new),
+    onExists = \v e var new -> if v == var then Exists v new else Exists v (e var new),
+    onSizeOf = \arr var new -> SizeOf (arr var new),
+    onRepBy = \arr index val var new -> RepBy (arr var new) (index var new) (val var new),
+    onCond = \guard e1 e2 var new -> Cond (guard var new) (e1 var new) (e2 var new),
+    onNewStore = \_ _ -> NewStore,
+    onDereference = \ptr _ _ -> Dereference ptr -- This probably is wrong 
+  } 
 
 {-
-necessities: 
+necessities:
 - WLP calculation (calculate the wlp of a given program)
     - possible also a wlp tree structure, so that we can handle conditionals
       and loops better
