9d18206897a170010801dbed3f52110d9fb1f878
Ruben <github@rubenbroere.com>
2025-10-08 11:33:41 +0200

    feat: Z3 invokation and wlp reduction

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,8 @@ module Main where
 import Algebra
 import GCLParser.GCLDatatype
 import GCLParser.Parser (parseGCLfile)
+import Z3.Monad hiding (substitute)
+import Control.Monad.IO.Class
 
 main :: IO ()
 main =
@@ -18,15 +20,52 @@ main =
       putStrLn "\nOriginal Statement:"
       print programStmt
 
-      let filteredStmt = transformAsserts programStmt
-      putStrLn "\nFiltered Statement (without asserts):"
-      print filteredStmt
+      -- This needs to be done for the feasibility checker
+      -- let filteredStmt = transformAsserts programStmt
+      -- putStrLn "\nFiltered Statement (without asserts):"
+      -- print filteredStmt
 
       let k = 3 -- The fixed point bound
-
-      let wlpFormula = wlp k filteredStmt (LitB True) -- assuming postcondition is true
+      let wlpFormula = reduceExpr $ wlp k programStmt (LitB True)
       putStrLn "\nWLP Formula:"
       print wlpFormula
+
+      evalZ3 $ do
+        ast <- exprToZ3 wlpFormula
+        str <- astToString ast
+        liftIO $ putStrLn ("\nZ3 AST String Representation:\n" ++ str)
+
+      putStrLn "\nAnalyzing WLP Formula with Z3:"
+      validateExpr wlpFormula >>= \case
+        True -> putStrLn "The WLP formula is valid."
+        False -> putStrLn "The WLP formula is not valid."
+
+validateExpr :: Expr -> IO Bool
+validateExpr expr = evalZ3 $ do
+  z3expr <- exprToZ3 $ OpNeg expr
+  assert z3expr
+  res <- check
+  case res of
+    Sat -> return False
+    Unsat -> return True
+    Undef -> error "Z3 returned UNKNOWN"
+
+analyzeExpr :: Expr -> IO ()
+analyzeExpr expr = evalZ3 $ do
+  z3expr <- exprToZ3 expr
+  assert z3expr
+  res <- check
+  case res of
+    Sat -> do
+      (_, mModel) <- getModel
+      case mModel of
+        Just mdl -> do
+          str <- modelToString mdl
+          liftIO $ putStrLn ("SAT\n" ++ str)
+        Nothing -> liftIO $ putStrLn "SAT\nNo model available."
+    Unsat -> liftIO $ putStrLn "UNSAT"
+    Undef -> liftIO $ putStrLn "UNKNOWN"
+
 
 -- Filter out any assert statements from the program and convert assumes to asserts
 transformAsserts :: Stmt -> Stmt
@@ -36,6 +75,40 @@ transformAsserts =
       { onAssert = const Skip,
         onAssume = Assert
       }
+
+reduceExpr :: Expr -> Expr
+reduceExpr = foldExpr reduceAlgebra
+
+reduceAlgebra :: ExprAlgebra Expr
+reduceAlgebra = defaultExprAlgebra {
+    onOpNeg = \e -> case e of
+      LitB b -> LitB (not b)
+      OpNeg inner -> inner -- Double negation elimination
+      _ -> OpNeg e,
+    onBinopExpr = \op e1 e2 -> case (op, e1, e2) of
+      (And, LitB True, r) -> r
+      (And, l, LitB True) -> l
+      (And, LitB False, _) -> LitB False
+      (And, _, LitB False) -> LitB False
+      (Or, LitB False, r) -> r
+      (Or, l, LitB False) -> l
+      (Or, LitB True, _) -> LitB True
+      (Or, _, LitB True) -> LitB True
+      (Implication, LitB False, _) -> LitB True
+      (Implication, _, LitB True) -> LitB True
+      (Implication, LitB True, r) -> r
+      (Implication, l, LitB False) -> OpNeg l
+      (Plus, _, LitI 0) -> e1
+      (Plus, LitI 0, _) -> e2
+      (Plus, LitI i, LitI j) -> LitI (i + j)
+      (Plus, BinopExpr Plus l1 (LitI i), LitI j) -> BinopExpr Plus l1 (LitI (i + j)) -- Flatten nested additions 
+      (Minus, BinopExpr Minus l1 (LitI i), LitI j) -> BinopExpr Minus l1 (LitI (i + j)) -- Flatten nested additions 
+      (Multiply, _, LitI 1) -> e1
+      (Multiply, LitI 1, _) -> e2
+      (Multiply, _, LitI 0) -> LitI 0
+      (Multiply, LitI 0, _) -> LitI 0
+      _ -> BinopExpr op e1 e2
+}
 
 wlp :: Int -> Stmt -> Expr -> Expr
 wlp k =
@@ -47,8 +120,8 @@ wlp k =
         onAssign = substitute,
         onAAssign = \_ _ _ -> error "WLP for array assignments not implemented yet",
         onDrefAssign = \_ _ -> error "WLP for dereference assignments not implemented yet",
-        onSeq = (.), -- function composition
-        onIfThenElse = \guard f1 f2 post -> opAnd (opImplication guard (f1 post)) (opImplication (OpNeg guard) (f2 post)),
+        onSeq = \s1 s2 post -> s1 (s2 post),
+        onIfThenElse = \guard s1 s2 post -> opAnd (opImplication guard (s1 post)) (opImplication (OpNeg guard) (s2 post)),
         onWhile = iterateWlpBounded k,
         onBlock = \_ f -> f,
         onTryCatch = \_ _ _ -> error "WLP for TryCatch not implemented yet"
@@ -58,29 +131,72 @@ wlp k =
 iterateWlpBounded :: Int -> Expr -> (Expr -> Expr) -> Expr -> Expr
 iterateWlpBounded 0 _ _ post = post
 iterateWlpBounded k guard f post =
-  opAnd (opImplication (OpNeg guard) post) (opImplication guard (f (iterateWlpBounded (k-1) guard f post)))
+  opAnd (opImplication (OpNeg guard) post) (opImplication guard (f (iterateWlpBounded (k - 1) guard f post)))
 
 substitute :: String -> Expr -> Expr -> Expr
 substitute var new post = foldExpr substituteAlgebra post var new
 
 substituteAlgebra :: ExprAlgebra (String -> Expr -> Expr)
-substituteAlgebra = ExprAlgebra
-  { onVar = \v var new -> if v == var then new else Var v,
-    onLitI = \i _ _ -> LitI i,
-    onLitB = \b _ _ -> LitB b,
-    onLitNull = \_ _ -> LitNull,
-    onParens = \e var new -> Parens (e var new),
-    onArrayElem = \arr index var new -> ArrayElem (arr var new) (index var new),
-    onOpNeg = \e var new -> OpNeg (e var new),
-    onBinopExpr = \op e1 e2 var new -> BinopExpr op (e1 var new) (e2 var new),
-    onForall = \v e var new -> if v == var then Forall v new else Forall v (e var new), -- Is this correct?
-    onExists = \v e var new -> if v == var then Exists v new else Exists v (e var new), -- Is this correct?
-    onSizeOf = \arr var new -> SizeOf (arr var new),
-    onRepBy = \arr index val var new -> RepBy (arr var new) (index var new) (val var new),
-    onCond = \guard e1 e2 var new -> Cond (guard var new) (e1 var new) (e2 var new),
-    onNewStore = \_ _ -> NewStore,
-    onDereference = \ptr _ _ -> Dereference ptr -- This probably is wrong 
-  }
+substituteAlgebra =
+  ExprAlgebra
+    { onVar = \v var new -> if v == var then new else Var v,
+      onLitI = \i _ _ -> LitI i,
+      onLitB = \b _ _ -> LitB b,
+      onLitNull = \_ _ -> LitNull,
+      onParens = \e var new -> Parens (e var new),
+      onArrayElem = \arr index var new -> ArrayElem (arr var new) (index var new),
+      onOpNeg = \e var new -> OpNeg (e var new),
+      onBinopExpr = \op e1 e2 var new -> BinopExpr op (e1 var new) (e2 var new),
+      onForall = \v e var new -> if v == var then Forall v new else Forall v (e var new), -- Is this correct?
+      onExists = \v e var new -> if v == var then Exists v new else Exists v (e var new), -- Is this correct?
+      onSizeOf = \arr var new -> SizeOf (arr var new),
+      onRepBy = \arr index val var new -> RepBy (arr var new) (index var new) (val var new),
+      onCond = \guard e1 e2 var new -> Cond (guard var new) (e1 var new) (e2 var new),
+      onNewStore = \_ _ -> NewStore,
+      onDereference = \ptr _ _ -> Dereference ptr -- This probably is wrong
+    }
+
+exprToZ3 :: Expr -> Z3 AST
+exprToZ3 = foldExpr exprToZ3Algebra
+
+exprToZ3Algebra :: ExprAlgebra (Z3 AST)
+exprToZ3Algebra =
+  ExprAlgebra
+    { onVar = \v -> do
+        sym <- mkStringSymbol v
+        mkIntVar sym,
+      onLitI = mkInteger . toInteger,
+      onLitB = mkBool,
+      onLitNull = mkInteger 0, -- Represent null as 0 for simplicity
+      onOpNeg = (>>= mkNot),
+      onBinopExpr = \x e1 e2 -> do
+        ast1 <- e1
+        ast2 <- e2
+        case x of
+          And -> mkAnd [ast1, ast2]
+          Or -> mkOr [ast1, ast2]
+          Implication -> mkImplies ast1 ast2
+          LessThan -> mkLt ast1 ast2
+          LessThanEqual -> mkLe ast1 ast2
+          GreaterThan -> mkGt ast1 ast2
+          GreaterThanEqual -> mkGe ast1 ast2
+          Equal -> mkEq ast1 ast2
+          Minus -> mkSub [ast1, ast2]
+          Plus -> mkAdd [ast1, ast2]
+          Multiply -> mkMul [ast1, ast2]
+          Divide -> mkDiv ast1 ast2
+          Alias -> error "Alias operation not supported in Z3 translation",
+
+      onParens = id,
+      onArrayElem = undefined,
+      onForall = undefined,
+      onExists = undefined,
+      onSizeOf = undefined,
+      onRepBy = undefined,
+      onCond = undefined,
+      onNewStore = undefined,
+      onDereference = undefined
+    }
 
 {-
 necessities:
